6172d7413cd7b597f62c72fc4bbbf91283fddd38
jprod123 <jprod123@gmail.com>
2017-06-26 14:47:32 -0400

    Formatted conditional block

--- wiimote/src/wiimote_controller.cpp
+++ wiimote/src/wiimote_controller.cpp
@@ -1602,10 +1602,11 @@ int main(int argc, char *argv[])
   int pair_timeout;
   ros::param::param<int>("~pair_timeout", pair_timeout, 5);
 
-  if (fed_addr) ROS_INFO("* * * Pairing with %s", g_wiimote_node->getBluetoothAddr());
-
-  else ROS_INFO("Searching for Wiimotes");
-
+  if (fed_addr){
+    ROS_INFO("* * * Pairing with %s", g_wiimote_node->getBluetoothAddr());
+  } else { ROS_INFO("Searching for Wiimotes");
+  }
+ 
   ROS_INFO("Allow all joy sticks to remain at center position until calibrated.");
 
   if (g_wiimote_node->pairWiimote(0, pair_timeout))
